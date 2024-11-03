1ad1c0327efe00b14b125d30d91ccab2ef559916
Büşranur Baydur <busranurbaydur@gmail.com>
2024-11-04 00:53:04 +0300

    Algoritma çalışma mantığı eklendi.

--- AlgorithmProject/Program.cs
+++ AlgorithmProject/Program.cs
@@ -7,6 +7,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Etkinlikler başlangıç saatlerine göre sıralanır.
+        //ilk etkinlik ve ondan sonraki etkinliğin yol süresi eklenerek çakışıp çakışmadığı kontrol edilir.
+        //Çakışma varsa öncelik sırası ile iki etkinlik arasında gidilecek etkinlik belirlenir. Seçilen etkinlik ve sıradaki etkinlik için de aynı durum işlenir. Bu durum çakışma olmayana kadar devam eder. Çakışma yoksa seçilen etkinlik gidilecek etkinliğe eklenir.
+        //Bu döngü sırayla tüm etkinlikler için yapılır.
+        
         var events = EventManager.GetEvents();
 
         var travelTimes = TravelTimeManager.GetTravelTimes();
